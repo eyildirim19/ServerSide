86fd840b8055b6d9471c59906337250fb10a7fc9
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-09-11 13:22:35 +0300

    htmlactionveviewresultornekleri..

--- G_PartialViews/Controllers/HomeController.cs
+++ G_PartialViews/Controllers/HomeController.cs
@@ -9,9 +9,29 @@ namespace G_PartialViews.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+
+        public ViewResult Index()
         {
-            return View();
+            return View(); // by metodun dönüş tipi ViewResulttır.
+        }
+
+        public PartialViewResult Action1()
+        {
+
+            return PartialView("BenimSayfam"); // Bu metodun dönüş tpi PartilViewResulttır..
+        }
+
+        // Not : actionResult bütün hepsinin atası olduğu için ActionResult dönüştipli metot tanımlamak yeterlidir..sadece gövdede ne döneceğine karar veriniz..
+
+        public ActionResult Eksın()
+        {
+            return PartialView("View");
+        }
+
+        public ActionResult Ogrenciler()
+        {
+            string isim = "Ekrem";
+            return PartialView("_ogrenciTemplate", isim);
         }
     }
 }
